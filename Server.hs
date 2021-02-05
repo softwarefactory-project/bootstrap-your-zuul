b5b8002349f1607413caa3bdb2f7216a885a9765
Tristan Cacqueray <tdecacqu@redhat.com>
2021-02-05 03:12:05 +0000

    Add micro server to render the configuration over http
    This change adds a scotty service to render the configuration
    using a REST endpoint.
    
    Change-Id: I69dc897c61c15f9916eecb31f2d53d515e8abe59

---
+++ Server.hs
@@ -0,0 +1,50 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/d4f19a218cbb15a242864a49f8b9f16fb7d48ec8.tar.gz
+#! nix-shell --pure -i runghc -p "haskellPackages.ghcWithPackages (p: [ p.dhall p.scotty ])"
+
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import Data.Aeson (FromJSON)
+import Data.Text.Lazy (pack)
+import Dhall (embed, inject, inputExpr)
+import Dhall.Core (Expr (App))
+import Dhall.JSON (dhallToJSON)
+import qualified Dhall.TH
+import Web.Scotty
+
+-- | Generate Haskell Type from Dhall Type, see: https://hackage.haskell.org/package/dhall-1.38.0/docs/Dhall-TH.html
+Dhall.TH.makeHaskellTypes
+  [ Dhall.TH.SingleConstructor "Connections" "MakeConnections" "./BootstrapYourZuul/JsonConfig/Connections.dhall",
+    Dhall.TH.SingleConstructor "Config" "MakeConfig" "./BootstrapYourZuul/JsonConfig/Type.dhall"
+  ]
+
+-- | Enable loading the Config from JSON
+instance FromJSON Connections
+
+instance FromJSON Config
+
+main :: IO ()
+main = do
+  -- Load the `toConfig` function
+  toConfig <- inputExpr "./BootstrapYourZuul/JsonConfig/toConfig.dhall"
+  -- Load the `render` function
+  render <- inputExpr "./BootstrapYourZuul/render.dhall"
+  -- Start web service
+  scotty 3000 $
+    -- Add POST handler for /
+    post "/" $ do
+      -- Read Config from request body
+      (config :: Config) <- jsonData
+      -- Convert the Config to a Dhall expression
+      let configExpr = embed inject config
+      -- Call the `render` function
+      let renderExpr = App render $ App toConfig $ configExpr
+      -- Convert back the result to JSON
+      case dhallToJSON renderExpr of
+        Left err -> raise (pack (show err))
+        Right v -> json v
